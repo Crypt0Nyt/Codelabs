ca72e04612c46a2da8869458afc9b20b6656be6f
Hrithik Rohilla <59164922+Crypt0Nyt@users.noreply.github.com>
2021-05-12 23:12:27 +0530

    Hello Toast App

---
+++ HelloToast/app/src/main/java/com/streamliners/hellotoast/MainActivity.java
@@ -0,0 +1,34 @@
+package com.streamliners.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private int count = 0;
+    private TextView showCount;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showCount = (TextView) findViewById(R.id.show_count);
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT);
+        toast.show();
+
+
+    }
+
+    public void countUp(View view) {
+        ++count;
+        if (showCount != null)
+            showCount.setText(Integer.toString(count));
+    }
+}
